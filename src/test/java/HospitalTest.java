c0328533bfec14ccd1d6de70efbd72274cc71295
mateofrito <mattfry@outlook.com>
2019-02-08 13:47:14 -0500

    Meaningful message we hope works

--- src/test/java/HospitalTest.java
+++ src/test/java/HospitalTest.java
@@ -1,4 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
 
 public class HospitalTest {
+	
+	@Before
+		public void setup() {
+		underTest = new Employee("Jim");
+	}
+	
+	@Test
+	
+	public void addemployee() {
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 
 }
