f3a90186a6915e7b4e2b368eefa4d97130f6bd1a
mateofrito <mattfry@outlook.com>
2019-02-09 17:21:30 -0500

    stuck!

--- src/test/java/Patient.java
+++ src/test/java/Patient.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+
 
 public class Patient {
 	
@@ -6,44 +6,36 @@ public class Patient {
 	private String patientID;
 	private int healthPoints;
 	private int bloodLevel;
-	
-	
-	
 
 	
-	
-	
-
 	public Patient(String patientName, String patientID) {
 		this.patientName = patientName;
 		this.patientID = patientID;
 		this.healthPoints = 100;
 		this.bloodLevel = 100;
+		
 	}
-	public Patient(int bloodLevel) {
-		this.bloodLevel = bloodLevel;
 	
-		
-		
+	
+	
+	
+	public int getBloodLevel() {
+		return bloodLevel;
 	}
+	
 
 	public String patientID() {
-		
 		return patientID;
 	}
 
 	public String patientName() {
-		
 		return patientName;
 	}
 
-	public int checkBlood() {
-		
-		return bloodLevel;
-	}
+	
 
 	public int changeBlood(int bloodAdjustment) {
-		this.bloodLevel += bloodAdjustment;
+		
 		return bloodAdjustment;
 		// TODO Auto-generated method stub
 		
